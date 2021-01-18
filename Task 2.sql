bcd7ed4980e4da82e38585eecba0c4489b0f7fdd
Jonas hofmann <Jonash871j@gmail.com>
2021-01-18 11:08:32 +0100

    Commit

---
+++ Task 2.sql
@@ -0,0 +1,8 @@
+CREATE TABLE table_duck_audits (
+	 id SERIAL PRIMARY KEY,
+	 name TEXT NOT NULL,
+	 notes TEXT NOT NULL,
+	 show TEXT NOT NULL,
+	 studio TEXT NOT NULL,
+	 changed_on TIMESTAMP(6) NOT NULL
+);
---
+++ Task 3.sql
@@ -0,0 +1,37 @@
+-- Creates function
+-- The functions that gets called when log_data gets trigged
+CREATE OR REPLACE FUNCTION TF_log_data()
+RETURNS TRIGGER AS
+$$
+BEGIN
+	-- Adds the old data from the column that changed into duck_audits table
+	INSERT INTO table_duck_audits(name, notes, show, studio, changed_on)
+	VALUES(OLD.name, OLD.notes, OLD.show, OLD.studio, CURRENT_TIMESTAMP);
+	RETURN OLD;
+END;
+$$
+LANGUAGE PLPGSQL;
+
+-- Creates trigger
+-- Gets called every time a column gets updated in duck table
+CREATE TRIGGER TR_log_data
+AFTER UPDATE ON table_duck
+FOR EACH ROW
+EXECUTE FUNCTION TF_log_data();
+
+-- Updates duck table
+UPDATE table_duck
+SET name = 'test'
+WHERE id = 1;
+
+UPDATE table_duck
+SET name = 'dummy'
+WHERE id = 43;
+
+UPDATE table_duck
+SET name = 'dummy2'
+WHERE id = 32;
+
+-- Select
+SELECT * FROM table_duck_audits
+
